4da0712806bdbadf334ca0b6a71237bacc592d44
Nathan <Tolbert>
2021-05-13 13:33:09 -0400

    05132021 -- Branch Parity Fixes #2 --
    More additions to dev to bring it closer to 'development' branch; Added package-info.java to both model classes - copy paste from other branch ; copy paste Email.java from other branch ;

---
+++ src/main/java/com/mdspicesmicro/userprofileservice/model/user_aggregate/Email.java
@@ -0,0 +1,48 @@
+package com.mdspicesmicro.userprofileservice.model.user_aggregate;
+
+public class Email {
+	
+	private final String cleanedEmail;
+	
+	private String userId;
+	
+	private String domain;
+	
+	private String suffix;
+	
+	
+	
+	
+	
+	public Email(String userId, String domain, String suffix) {
+		super();
+		this.userId = despace(userId);
+		this.domain = despace(domain);
+		this.suffix = despace(suffix);
+		if (suffix.length() == 3) {
+			this.cleanedEmail = userId + "@" + domain +"." + suffix;
+		}
+	}
+
+
+
+
+
+	private String despace(String str) {
+		
+		char[] arr =  str.toCharArray();
+		String res = "";
+		for (char c : arr) {
+			if(Character.isWhitespace(c)) {
+				continue;
+			}else {
+				res += c;
+			}		
+		};
+		return res;
+		
+	}
+	
+	
+
+}
---
+++ src/main/java/com/mdspicesmicro/userprofileservice/model/user_aggregate/package-info.java
@@ -0,0 +1,39 @@
+package com.mdspicesmicro.userprofileservice.model.user_aggregate;
+
+/*
+ *  
+ *  - The purpose of this package is to serve as a container for the User aggregate and all it's classes.
+ * 
+ *         LIST AND BRIEF DESCRIPTIONS OF MEMBER CLASSES:
+ *      
+ *       
+ *             +  Address
+ *             
+ *             +  Email
+ *             
+ *             +  (Enum) DeviceType
+ *             
+ *             +  Device
+ *             
+ *             +  (Enum) Gender
+ *                         
+ *             +  User <<aggregate root>>
+ *             
+ *             +  UserActivityMetrics
+ *             
+ *             +  UserContactInfo
+ *             
+ *             +  (Enum) UserRole 
+ *             
+ *             +  UserStatus
+ *             
+ *             +  UserDevices
+ *             
+ *             +  UserDetails
+ *             
+ *             +  (interface) GetUserAccountsMembersService
+ *             
+ *             +  GetUserAccountsMembersService
+ * 
+ */
+
