05237546f3affe562fa5529b09219abd5ada753d
Nathan <Tolbert>
2021-06-13 01:37:42 -0400

    06132021 -- UsrAcctSvc && Impl of ''''
    Recreated interface (?????) and added params to build method of impl

--- src/main/java/com/mdspicesmicro/userprofileservice/service/ImplUserAccountService.java
+++ src/main/java/com/mdspicesmicro/userprofileservice/service/ImplUserAccountService.java
@@ -1,18 +1,20 @@
 package com.mdspicesmicro.userprofileservice.service;
 
 import com.mdspicesmicro.userprofileservice.model.account_aggregate.Account;
+import com.mdspicesmicro.userprofileservice.model.account_aggregate.Credentials;
 import com.mdspicesmicro.userprofileservice.model.account_aggregate.UserAccountPreferences;
+import com.mdspicesmicro.userprofileservice.model.account_aggregate.UserProfileInfo;
 
 public class ImplUserAccountService implements UserAccountService {
 
 	
-	public Account createNewAccountForUnregisteredUser(String userId) {
+	public Account createNewAccountForUnregisteredUser(Long userId, Credentials cred, UserProfileInfo info, UserAccountPreferences acctPrefs) {
 		// TODO
 		Account acct = Account.builder()
-				.userId(null)
-				.credentials(null)
-				.userProfileInfo(null)
-				.acctPrefs(null)
+				.userId(userId)
+				.credentials(cred)
+				.userProfileInfo(info)
+				.acctPrefs(acctPrefs)
 				.build();
 		return acct;
 	}
