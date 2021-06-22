425a3fd77d8cfa35d1bd2fbaa2ebcfd2e770bc2f
Nathan <Tolbert>
2021-06-22 06:06:00 -0400

    06222021 -- AcctState.java --
    Added methods to AcctState for setting creation date and getting Acct
    age;

--- src/main/java/com/mdspicesmicro/userprofileservice/model/account_aggregate/AccountState.java
+++ src/main/java/com/mdspicesmicro/userprofileservice/model/account_aggregate/AccountState.java
@@ -1,5 +1,13 @@
 package com.mdspicesmicro.userprofileservice.model.account_aggregate;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import javax.persistence.Entity;
 import javax.persistence.MapsId;
 
@@ -19,9 +27,10 @@ class AccountState {
   
     AccountState(Long id) {
     	this.accountId = id;
-    	this.ageInDays = 0;
+    	//this.ageInDays = 0;
     	this.pwdStrength = null;
     	this.status = AccountStatus.PENDING;
+    	this.createdDate = LocalDate.now();
     }
 
     @Id
@@ -30,13 +39,30 @@ class AccountState {
     private Long accountId;
     
     @Column(value="age_in_days")
-    private int ageInDays;
+    private LocalDate createdDate;
     
     @Column(value="pwd_strength")
     private String pwdStrength;
     
     @Column(value="status")
     private AccountStatus status;
+    
+    
 
-
+    Period getPeriodOfExistance() {
+    	LocalDate now = LocalDate.from(Instant.now());
+    	Period timePassed = Period.between(createdDate, now);
+    	return timePassed;
+    }
+    
+    String getPeriodAsString(){
+    	Period p = getPeriodOfExistance();   			    	
+    	return p.toString();
+    }
+    
+    int getAgeInDays() {
+    	LocalDate now = LocalDate.now();
+    	return (int) ChronoUnit.DAYS.between(createdDate, now);
+    }
+    
 }
