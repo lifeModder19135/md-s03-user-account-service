445e88090779b74c02011b867f2894e65b2cb7f9
Nathan <Tolbert>
2021-06-27 03:25:58 -0400

    06272021 -- AcctStateRepository --
    Deleted repository to inner aggregate class. Access via traversal only!

--- src/main/java/com/mdspicesmicro/userprofileservice/model/repositories/AccountStateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mdspicesmicro.userprofileservice.model.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mdspicesmicro.userprofileservice.model.account_aggregate.AccountStatus;
-
-public interface AccountStateRepository extends JpaRepository<AccountState, Long> {
-
-}
